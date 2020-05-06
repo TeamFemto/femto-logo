1ca2fcae3cfc81f8d7de713f38388f15da4b34a5
NessMC <contact@nessmc.fr>
2020-05-06 18:37:56 +0200

    ✨ Femto logo support in generation.

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,8 @@ const FEMTO_INPUTS = document.querySelectorAll('.femto-input'),
       GENERATOR    = document.getElementsByClassName('femto-generator')[0],
       NAME         = GENERATOR.getElementsByClassName('femto-name')[0],
       DOWNLOAD     = document.getElementsByClassName('femto-link')[0]
+let   DF_WEIGHT    = 6,
+      DF_IMAGE     = 128
 
 /*/////////////////////////////
         LOGO GENERATION
@@ -13,7 +15,23 @@ const FEMTO_INPUTS = document.querySelectorAll('.femto-input'),
 
 FEMTO_INPUTS.forEach(input => {
   input.addEventListener('keyup', () => {
-    NAME.innerHTML = input.value
+    NAME.innerHTML = ''
+    NAME.style.fontSize = DF_WEIGHT - (input.value.split('').length / 4) + 'rem'
+    const LETTERS = input.value.split('')
+    LETTERS.forEach(element => {
+      const SPAN = document.createElement('span')
+      SPAN.classList.add('femto-letter')
+      if (element.toLowerCase() === 'o') {
+        const IMAGE = document.createElement('img')
+        IMAGE.src = 'assets/img/femto.png'
+        IMAGE.alt = ''
+        SPAN.appendChild(IMAGE)
+        IMAGE.style.width = DF_IMAGE - (input.value.split('').length * 5) + 'px'
+      } else {
+        SPAN.innerHTML = element
+      }
+      NAME.appendChild(SPAN)
+    })
   })
 })
 
